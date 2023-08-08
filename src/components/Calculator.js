54ada465e005975f699f875bb08512d412d727c6
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-08-08 15:54:20 -0300

    finish calculator structure and styles

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,43 +1,48 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import PropTypes from 'prop-types';
+
 function CalcItem(props) {
-  const item = props;
+  const { item, itemClass } = props;
   return (
-    <button type="button" className="symbol">
-      {item}
-    </button>
-  );
-}
-function CalcItem0(props) {
-  const item = props;
-  return (
-    <button type="button" className="symbol0">
+    <button type="button" className={itemClass}>
       {item}
     </button>
   );
 }
 
-function ResultBar() {
+CalcItem.propTypes = {
+  item: PropTypes.string.isRequired,
+  itemClass: PropTypes.string.isRequired,
+};
+
+function Calculator() {
   return (
     <article className="calculator">
       <input className="resultBar" placeholder="0" />
       <div className="calculatorItems">
-        {CalcItem('10')}
-        {CalcItem('10')}
-        {CalcItem('10')}
-        {CalcItem('10')}
-        {CalcItem('10')}
-        {CalcItem('10')}
-        {CalcItem('10')}
-        {CalcItem('10')}
-        {CalcItem('10')}
-        {CalcItem('10')}
-        {CalcItem('10')}
-        {CalcItem('10')}
-        {CalcItem0('0')}
-        {CalcItem('8')}
-        {CalcItem('8')}
+        <CalcItem item="AC" itemClass="symbol" />
+        <CalcItem item="+/-" itemClass="symbol" />
+        <CalcItem item="%" itemClass="symbol" />
+        <CalcItem item="÷" itemClass="symbolOrange" />
+        <CalcItem item="7" itemClass="symbol" />
+        <CalcItem item="8" itemClass="symbol" />
+        <CalcItem item="9" itemClass="symbol" />
+        <CalcItem item="X" itemClass="symbolOrange" />
+        <CalcItem item="4" itemClass="symbol" />
+        <CalcItem item="5" itemClass="symbol" />
+        <CalcItem item="6" itemClass="symbol" />
+        <CalcItem item="-" itemClass="symbolOrange" />
+        <CalcItem item="1" itemClass="symbol" />
+        <CalcItem item="2" itemClass="symbol" />
+        <CalcItem item="3" itemClass="symbol" />
+        <CalcItem item="+" itemClass="symbolOrange" />
+        <CalcItem item="0" itemClass="symbol0" />
+        <CalcItem item="." itemClass="symbol" />
+        <CalcItem item="=" itemClass="symbolOrange" />
+
       </div>
     </article>
   );
 }
 
-export default ResultBar;
+export default Calculator;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Example from './components/Calculator';
+import Calculator from './components/Calculator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Example />,
+  <Calculator />,
 );
 
 // If you want to start measuring performance in your app, pass a function
