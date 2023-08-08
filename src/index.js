c4beceb2521d55970884eef42feef7c7612ea3af
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-08-08 16:13:48 -0300

    solve linters issue

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// eslint-disable-next-line import/extensions
+import App from './App.js';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
