e4113c0f3f891b2c91c0c61d8d3bfbc1c338e217
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-08-10 13:53:05 -0300

    add API fetch

--- src/components/quotes.js
+++ src/components/quotes.js
@@ -1,7 +1,26 @@
-// import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 const ComputerQuotes = () => {
-  console.log('0');
+  const [quote, setQuote] = useState([]);
+
+  useEffect(() => {
+    const fetchQuote = async () => {
+      const category = 'happiness';
+      const result = await fetch(`https://api.api-ninjas.com/v1/quotes?category=${category}`, {
+        method: 'GET',
+        headers: { 'X-Api-Key': 'St8XblZ+dlsMEoh/ukYC8Q==JuLsC7tTNp0OCP2V' },
+        contentType: 'application/json',
+      });
+      const json = await result.json();
+      console.log(json);
+      setQuote(json[0].quote);
+    };
+    fetchQuote();
+  }, []);
+
+  return (
+    <p>{quote}</p>
+  );
 };
 
 export default ComputerQuotes;
