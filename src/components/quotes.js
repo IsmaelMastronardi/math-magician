54a0a0a7b7d596c672f3986a32657f99ce9c2124
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-08-10 14:09:24 -0300

    add loading state, started error state

--- src/components/quotes.js
+++ src/components/quotes.js
@@ -2,22 +2,38 @@ import { useState, useEffect } from 'react';
 
 const ComputerQuotes = () => {
   const [quote, setQuote] = useState([]);
+  const [loadingComment, setLoadingComment] = useState(true);
+  const [errorFound, setErrorFound] = useState(null);
 
   useEffect(() => {
-    const fetchQuote = async () => {
-      const category = 'happiness';
-      const result = await fetch(`https://api.api-ninjas.com/v1/quotes?category=${category}`, {
-        method: 'GET',
-        headers: { 'X-Api-Key': 'St8XblZ+dlsMEoh/ukYC8Q==JuLsC7tTNp0OCP2V' },
-        contentType: 'application/json',
-      });
-      const json = await result.json();
-      console.log(json);
-      setQuote(json[0].quote);
-    };
-    fetchQuote();
+    try {
+      const fetchQuote = async () => {
+        const category = 'happiness';
+        const result = await fetch(`https://api.api-ninjas.com/v1/quotes?category=${category}`, {
+          method: 'GET',
+          headers: { 'X-Api-Key': 'St8XblZ+dlsMEoh/ukYC8Q==JuLsC7tTNp0OCP2V' },
+          contentType: 'application/json',
+        });
+        const json = await result.json();
+        console.log(json);
+        setQuote(json[0].quote);
+        setLoadingComment(false);
+      };
+      fetchQuote();
+    } catch (error) {
+      setErrorFound(true);
+    }
   }, []);
-
+  if (loadingComment) {
+    return (
+      <p>Loading Comment...</p>
+    );
+  }
+  if (errorFound) {
+    return (
+      <p>Sorry, an error has been found. Try again</p>
+    );
+  }
   return (
     <p>{quote}</p>
   );
