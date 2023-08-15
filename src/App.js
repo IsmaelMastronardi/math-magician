0bceae12d4ecf7edce56dc47d43c50537c07c095
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-08-15 18:43:26 -0300

    add routers, add HomePage.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
+import { Route, Routes, Link } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import ComputerQuotes from './components/quotes';
+import HomePage from './components/Home';
+import './App.css';
 
 function App() {
   return (
     <>
-      <Calculator />
-      <ComputerQuotes />
+      <nav className="pageLinks">
+        <ul className="navList">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/calculator">Calculator</Link>
+          </li>
+          <li>
+            <Link to="/quotes">Quotes</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quotes" element={<ComputerQuotes />} />
+      </Routes>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 // eslint-disable-next-line import/extensions
 import App from './App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
 );
